ea3254492b9cc2d7e25642cff74fba34ea6496dd
Ilya Tribusean <slash3b@gmail.com>
2025-10-15 21:33:53 +0300

    jaeger stuff

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,6 @@
 set -e
 
 # ----------------------------------------------------------------------------------------------
-# todo: delete or fix
-# kubernetes-dashboard repository
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-
-# ----------------------------------------------------------------------------------------------
 # Cert-Manager (required for OpenTelemetry Operator)
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
